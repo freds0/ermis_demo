dbb6df67cdb8977a4370e254605158a326fbfc1a
Lucas Rafael <gris@alunos.utfpr.edu.br>
2023-12-10 11:16:32 -0300

    Adiciona suporte para FasterWhisper

--- stt/api.py
+++ stt/api.py
@@ -1,19 +1,45 @@
 from flask import Flask, request, jsonify
-from whisper import SpeechToText
+from whisper import Whisper, FasterWhisper
+import traceback
 
 app = Flask(__name__)
 
-@app.route('/speech-to-text', methods=['POST'])
-def speech_to_text():
+# stt = Whisper()
+stt = FasterWhisper()
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
     data = request.get_json()
 
     if 'audio' not in data:
         return jsonify({'error': 'Audio data missing in the request body'}), 400
 
     audio_filepath = data['audio']
-    stt = SpeechToText()
     audio_data = stt.load_filepath(audio_filepath)
-    transcribed_text = stt.transcribe(audio_data)
+    try:
+        transcribed_text = stt.transcribe(audio_data)
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': f'Failed to transcribe the audio:  {e}'}), 500
+    if transcribed_text:
+        return jsonify({'text': transcribed_text}), 200
+    else:
+        return jsonify({'error': 'Failed to transcribe the audio'}), 500
+    
+@app.route('/translate', methods=['POST'])
+def translate():
+    data = request.get_json()
+
+    if 'audio' not in data:
+        return jsonify({'error': 'Audio data missing in the request body'}), 400
+
+    audio_filepath = data['audio']
+    audio_data = stt.load_filepath(audio_filepath)
+    try:
+        transcribed_text = stt.translate(audio_data)
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': f'Failed to transcribe the audio:  {e}'}), 500
     if transcribed_text:
         return jsonify({'text': transcribed_text}), 200
     else:
--- stt/whisper.py
+++ stt/whisper.py
@@ -1,30 +1,116 @@
+import os
 import torch
 import torchaudio
 import torchaudio.transforms as T
 from stt_interface import SpeechToTextInterface
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from faster_whisper import WhisperModel
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class SpeechToText(SpeechToTextInterface):
 
-    def __init__(self, model_id: str = "openai/whisper-large-v2"):
+class Whisper(SpeechToTextInterface):
+    def __init__(self, model_id: str = os.getenv("ASR_MODEL", "openai/whisper-base")):
         self.processor = WhisperProcessor.from_pretrained(model_id)
-        self.model = WhisperForConditionalGeneration.from_pretrained(model_id).to(device)
-        self.forced_decoder_ids = self.processor.get_decoder_prompt_ids(language="english", task="transcribe")
+        self.model = WhisperForConditionalGeneration.from_pretrained(model_id).to(
+            device
+        )
         self.sr_target = 16000
 
     def load_filepath(self, filepath: str = None) -> torch.Tensor:
         waveform, sample_rate = torchaudio.load(filepath)
+        if waveform.shape[0] > 1:
+            waveform = waveform.mean(0, keepdim=True)
         if sample_rate != self.sr_target:
             resampler = T.Resample(sample_rate, self.sr_target)
             waveform = resampler(waveform)
         return waveform.squeeze()
 
-    def transcribe(self, waveform: torch.Tensor) -> str:
-        #waveform = self.load_filepath(input_filepath)
-        input_features = self.processor(waveform, sampling_rate=self.sr_target, return_tensors="pt").input_features 
+    def transcribe(self, audio_data, language="en") -> str:
+        if isinstance(audio_data, str):
+            audio_data = self.load_filepath(audio_data)
+        input_features = self.processor(
+            audio_data, sampling_rate=self.sr_target, return_tensors="pt"
+        ).input_features
         with torch.no_grad():
-            predicted_ids = self.model.generate(input_features.to(device), forced_decoder_ids=self.forced_decoder_ids)            
-        transcription = self.processor.batch_decode(predicted_ids, skip_special_tokens=True)
+            predicted_ids = self.model.generate(
+                input_features.to(device),
+                forced_decoder_ids=self.processor.get_decoder_prompt_ids(
+                    language=language, task="transcribe"
+                ),
+            )
+        transcription = self.processor.batch_decode(
+            predicted_ids, skip_special_tokens=True
+        )
         return transcription[0]
+
+    def translate(self, audio_data, target_language="en"):
+        if isinstance(audio_data, str):
+            audio_data = self.load_filepath(audio_data)
+        input_features = self.processor(
+            audio_data, sampling_rate=self.sr_target, return_tensors="pt"
+        ).input_features
+        with torch.no_grad():
+            predicted_ids = self.model.generate(
+                input_features.to(device),
+                forced_decoder_ids=self.processor.get_decoder_prompt_ids(
+                    language=target_language, task="translate"
+                ),
+            )
+        translation = self.processor.batch_decode(
+            predicted_ids, skip_special_tokens=True
+        )
+        return translation[0]
+
+
+class FasterWhisper(SpeechToTextInterface):
+    def __init__(self, model_id: str = os.getenv("ASR_MODEL", "base")):
+        model_path = os.getenv(
+            "ASR_MODEL_PATH", os.path.join(os.path.expanduser("~"), ".cache", "whisper")
+        )
+        if torch.cuda.is_available():
+            device = "cuda"
+            model_quantization = os.getenv("ASR_QUANTIZATION", "float32")
+        else:
+            device = "cpu"
+            model_quantization = os.getenv("ASR_QUANTIZATION", "int8")
+        self.model = WhisperModel(
+            model_id,
+            device=device,
+            compute_type=model_quantization,
+            download_root=model_path,
+        )
+        self.sr_target = 16000
+
+    def load_filepath(self, filepath: str = None) -> torch.Tensor:
+        waveform, sample_rate = torchaudio.load(filepath)
+        if waveform.shape[0] > 1:
+            waveform = waveform.mean(0, keepdim=True)
+        if sample_rate != self.sr_target:
+            resampler = T.Resample(sample_rate, self.sr_target)
+            waveform = resampler(waveform)
+        return waveform.squeeze().detach().cpu().numpy()
+
+    def transcribe(self, audio_data, language="en") -> str:
+        if isinstance(audio_data, str):
+            audio_data = self.load_filepath(audio_data)
+        segment_generator, info = self.model.transcribe(
+            audio_data, beam_size=5, language=language, task="transcribe"
+        )
+        segments = []
+        text = ""
+        for segment in segment_generator:
+            segments.append(segment)
+            text = text + segment.text
+        return text
+
+    def translate(self, audio_data, target_language="en"):
+        segment_generator, info = self.model.transcribe(
+            audio_data, beam_size=5, language=target_language, task="translate"
+        )
+        segments = []
+        text = ""
+        for segment in segment_generator:
+            segments.append(segment)
+            text = text + segment.text
+        return text
